a19d38a4221361c14085843b32e9120e99d51352
unknown <rurato2002@gmail.com>
2023-10-01 13:43:39 -0300

    alteracao pastas

--- TCC2/Scripts/menu.js
+++
@@ -1,5 +0,0 @@
-const logoutBtn = document.getElementById('logoutBtn');
-
-logoutBtn.addEventListener('click', () => {
-  window.location.href = 'login.html';
-});
--- TCC2/Scripts/script.js
+++
@@ -1,6 +0,0 @@
-// Não há mais necessidade de código de login
-const logoutBtn = document.getElementById('logoutBtn');
-
-logoutBtn.addEventListener('click', () => {
-  // Ação de logout aqui, se necessário
-});
